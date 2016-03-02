006a4dffe965036bd4ed679725b4236016160c01
han <1694188633@qq.com>
2016-03-02 09:09:31 +0800

    自定义UI控件...

---
+++ app/src/main/java/com/xiaosai/uicustomviews/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xiaosai.uicustomviews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/3/2 0002.
+ */
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
